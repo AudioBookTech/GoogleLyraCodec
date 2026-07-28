7a27b0239a5f3ddb8a4939bbeb062f1fa3ada73b
Konstantin Shramko <apsupport@ukr.net>
2026-07-28 07:21:06 +0300

    Update encode_example.cmd

--- vaudio_lyra_Windows/encode_example.cmd
+++ vaudio_lyra_Windows/encode_example.cmd
@@ -1,1 +1,1 @@
-E:\Temp\lyra\encoder_main.exe  --bitrate=3200 --output_dir=e:\temp\encoded --model_path=E:\Temp\lyra\model_coeffs --input_path=E:\Temp\wav\100.wav
+E:\Temp\encoder_main.exe  --bitrate=3200 --output_dir=e:\temp\ --input_path=E:\Temp\101.wav
